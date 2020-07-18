80d64e20725a60c4e9ceb21ddb45de9a34b5babd
Vince P <ianparulan@gmail.com>
2020-07-17 20:55:33 -0400

    changed footer

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 /** @jsx jsx */
 import { Container, jsx } from "theme-ui"
+import { mediaQueries } from '../gatsby-plugin-theme-ui/tokens'
 
 export const Footer = (props) => {
   return (
     <footer
       sx={{
-        height: `64px`,
+        minHeight: `256px`,
+        bg: `black`,
+        position: `relative`,
+        overflow: `hidden`,
       }}
       className={props.className}
     >
@@ -19,13 +23,60 @@ export const Footer = (props) => {
           height: `100%`,
         }}
       >
-        <div>
+        <div
+          sx={{
+            position: `absolute`,
+            right: 4,
+            top: 5,
+          }}
+        >
+          <span
+            sx={{
+              display: `block`,
+              color: `white`,
+              opacity: 0.1,
+              lineHeight: 0.9,
+              fontWeight: `bold`,
+              fontSize: 5,
+              [mediaQueries.lg]: {
+                fontSize: `128px`,
+              },
+            }}
+          >
+            WebDevPhotos
+          </span>
+        </div>
+        <div
+          sx={{
+            color: `white`,
+            zIndex: 1,
+            position: `absolute`,
+            bottom: 4,
+            opacity: 0.75,
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>,{` & `}
-          <a href="https://www.sanity.io">Sanity</a>.
+          <a
+            href="https://www.gatsbyjs.org"
+            sx={{
+              color: `inherit`,
+            }}
+          >
+            Gatsby
+          </a>
+          ,{` & `}
+          <a
+            href="https://www.sanity.io"
+            sx={{
+              color: `inherit`,
+            }}
+          >
+            Sanity
+          </a>
+          .
         </div>
       </Container>
     </footer>
-  );
+  )
 }
